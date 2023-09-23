a2942729f51ebf45fbb7429f74dbe6b0c51e4ef1
pepeballesterosgualda <134203798+pepeballesterosgualda@users.noreply.github.com>
2023-09-23 08:34:24 +0200

    Add files via upload

---
+++ app-task/database.php
@@ -0,0 +1,13 @@
+
+<?php
+
+$connection = mysqli_connect(
+  'localhost', 'root', 'password', 'tasks-database'
+);
+
+// for testing connection
+#if($connection) {
+#  echo 'database is connected';
+#}
+
+?>
